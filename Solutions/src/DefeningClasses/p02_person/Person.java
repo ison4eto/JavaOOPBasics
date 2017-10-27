6f63865451a65dc0b13354042463f87834506d42
iskra <ison4eto635@gmail.com>
2017-10-27 23:33:53 +0300

    Add p02_person.

--- Solutions/src/DefeningClasses/p02_person/Person.java
+++ Solutions/src/DefeningClasses/p02_person/Person.java
@@ -1,5 +1,30 @@
 package DefeningClasses.p02_person;
 
-public class Person {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Person {
+    private String name;
+    private int age;
+    private List<BankAccount> accounts;
+
+    public Person(String name, int age, List<BankAccount> accounts) {
+        this.name = name;
+        this.age = age;
+        this.accounts = accounts;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.accounts = new ArrayList<>();
+    }
+
+    public Person() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public double getBalance(){
+        return this.accounts.stream().mapToDouble(BankAccount::getBalance).sum();
+    }
 }
